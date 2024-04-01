15882514c4c923cf051f4afbe977f150039016b4
sokoeurn88 <sokoeurn.chhaya@gmail.com>
2024-04-01 11:12:43 -0500

    cucumber gherkin java

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,31 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class LoginSteps {
+
+	@Given("user is on login page")
+	public void user_is_on_login_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@When("user enters username and password")
+	public void user_enters_username_and_password() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@And("clicks on login button")
+	public void clicks_on_login_button() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+	@Then("user navigated to home page")
+	public void user_navigated_to_home_page() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new io.cucumber.java.PendingException();
+	}
+
+}
